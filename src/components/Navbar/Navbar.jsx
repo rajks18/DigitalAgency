b0cc805e04de2ed9e6534be78f0dcad6b16dba66
Rajkumar Sarker <sardonic100@gmail.com>
2026-01-12 03:05:22 +0600

    Add dynamic navigation links to Navbar component
    Introduced a MenuLinks array and mapped it to render navigation links in the Navbar. Improved structure and styling for the logo and navigation sections.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,55 @@
-import React from 'react'
-import logo from "../../assets/website/logo.svg"
+import React from "react";
+import logo from "../../assets/website/logo.svg";
+
+const MenuLinks = [
+    {
+        id : 1,
+        name : "About",
+        link : "#about",
+    },
+    {
+        id : 2,
+        name : "Services",
+        link : "#services",
+    },
+    {
+        id : 3,
+        name : "Projects",
+        link : "#projects",
+    },
+]
 const Navbar = () => {
   return (
     <>
-        <nav>
-            <div className='container'>
-                <div className='flex'>
-                    <div>
-                        <img src={logo} alt="Logo" className='w-5' />
-                        <a href="#" className=''>Digital Agency</a>
-                    </div>
-                </div>
+      <nav>
+        <div className="container py-3 sm:py-2">
+          <div className="flex">
+            {/* Logo Section */}
+            <div>
+              <a href="#" className="flex items-center gap-3">
+                <img src={logo} alt="Logo" className="w-5" />
+                <span className="text-2xl sm:text-3xl font-semibold">Digital Agency</span>
+              </a>
             </div>
-        </nav>
+            {/* NavLinks Section */}
+            <div>
+                <ul>
+                    {
+                        MenuLinks.map(({id, name, link}) => {
+                            return (
+                                <li key={id}>
+                                    <a href="">{Name}</a>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
