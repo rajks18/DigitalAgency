838ee69893528b71dd147b5c491e3d3f6a848fd8
Rajkumar Sarker <sardonic100@gmail.com>
2026-01-12 08:51:03 +0600

    Revamp Hero component layout and content
    Updated the Hero component with a new grid layout, improved styling for light/dark modes, and replaced placeholder text with a heading, description, and call-to-action button. These changes enhance the visual appeal and provide clearer messaging for users.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,15 +3,19 @@ import HeroImage from "../../assets/website/HeroImage.png";
 
 const Hero = () => {
   return (
-    <main className=''>
-        <div>
-            <div className=''>
+    <main className='bg-white dark:bg-gray-950 dark:text-white duration-300'>
+        <div className='container min-h-[620px] flex mt-10 sm:mt-0'>
+            <div className='grid grid-cols-2 gap-5 place-items-center'>
                 {/* Image Section */}
-                <div>
+                <div className='order-1 sm:order-2 relative'>
                     <img src={HeroImage} alt="Hero Image" />
                 </div>
                 {/* Text Content Section */}
-                <div>jjj</div>
+                <div>
+                    <h1>Building Brands in the <span>Digital Agency</span></h1>
+                    <p>Your partner in navigating the ever-evolving landscape of digital marketing. From conceptualization to execution, we craft tailored solutions that drive results and elevate your brand to new heights.</p>
+                    <button>Get Started</button>
+                </div>
             </div>
         </div>
     </main>
