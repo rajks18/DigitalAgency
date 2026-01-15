795861bfafb63888dd720fe612eaa5aad90b5c5e
Rajkumar Sarker <sardonic100@gmail.com>
2026-01-15 23:00:32 +0600

    Update ResponsiveMenu.jsx

--- src/components/Navbar/ResponsiveMenu.jsx
+++ src/components/Navbar/ResponsiveMenu.jsx
@@ -17,7 +17,7 @@ const ResponsiveMenu = ({showMenu}) => {
                 </div>
 
                 {/* Menu Section */}
-                <nav className='mt-14'>
+                <nav className='mt-12'>
                     <ul className='space-y-4 text-xl'>
                         {
                             MenuLinks.map(({ id, name, link }) => {
