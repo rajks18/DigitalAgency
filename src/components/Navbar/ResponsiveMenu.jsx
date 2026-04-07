f358de17ca3da450ed14b4ea25019730171a310b
Rajkumar Sarker <sardonic100@gmail.com>
2026-04-07 23:36:25 +0600

    Update ResponsiveMenu.jsx

--- src/components/Navbar/ResponsiveMenu.jsx
+++ src/components/Navbar/ResponsiveMenu.jsx
@@ -23,7 +23,7 @@ const ResponsiveMenu = ({showMenu}) => {
                             MenuLinks.map(({ id, name, link }) => {
                                 return (
                                     <li key = {id}>
-                                        <a href={link} className='mb-15 inline-block'>{name}</a>
+                                        <a href={link} className='mb-16 inline-block'>{name}</a>
                                     </li>
                                 )
                             })
