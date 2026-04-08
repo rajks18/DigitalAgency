40904e970e23c41f95f6e2647de1b9f6413afd7a
Rajkumar Sarker <sardonic100@gmail.com>
2026-04-08 23:42:43 +0600

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -58,7 +58,7 @@ const Navbar = () => {
             </div>
 
             {/* Mobile View */}
-            <div className="flex items-center gap-16 md:hidden">
+            <div className="flex items-center gap-4 md:hidden">
                 <DarkMode/>
                 {
                     showMenu ? (
